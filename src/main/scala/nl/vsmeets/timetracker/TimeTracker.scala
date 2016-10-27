8147b73963d6c920c7c77dfe3ced3759fd6971bb
Vincent Smeets <Vincent.VSmeets@GMail.com>
2016-10-27 21:16:29 +0200

    Create the tables.

--- src/main/scala/nl/vsmeets/timetracker/TimeTracker.scala
+++ src/main/scala/nl/vsmeets/timetracker/TimeTracker.scala
@@ -5,21 +5,24 @@ import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.event.LoggingReceive
 import nl.vsmeets.timetracker.database.DataSourceActor
-import nl.vsmeets.timetracker.database.DataSourceActor.DataSourceRequest
-import nl.vsmeets.timetracker.database.DataSourceActor.DataSourceResponse
+import nl.vsmeets.timetracker.database.FlywayActor
+import nl.vsmeets.timetracker.database.FlywayActor.Flyway
+import nl.vsmeets.timetracker.database.FlywayActor.Migrate
+import nl.vsmeets.timetracker.database.FlywayActor.Migrated
 
 object TimeTracker extends App {
   implicit val system = ActorSystem("TimeTracker")
 
   class MainActor extends Actor {
     def receive = LoggingReceive {
-      case DataSourceResponse(ds) =>
+      case Migrated =>
         system.terminate()
     }
   }
   val mainRef = system.actorOf(Props[MainActor], "Main")
 
   val dataSourceRef = DataSourceActor.createRef("database.data-source")
-  dataSourceRef.tell(DataSourceRequest, mainRef)
+  val flywayRef = FlywayActor.createRef(dataSourceRef, new org.flywaydb.core.Flyway with Flyway)
+  flywayRef.tell(Migrate, mainRef)
 
 }
