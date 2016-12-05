fd32902b8fb57e234724c596a168413e6a35dad1
Vincent Smeets <Vincent.VSmeets@GMail.com>
2016-12-05 19:26:36 +0100

    The akka version of main.

--- src/main/scala/nl/vsmeets/timetracker/TimeTracker.scala
+++ src/main/scala/nl/vsmeets/timetracker/TimeTracker.scala
@@ -1,5 +1,12 @@
 package nl.vsmeets.timetracker
 
+import akka.actor.ActorSystem
+import nl.vsmeets.timetracker.database.DatabaseActor
+import nl.vsmeets.timetracker.logic.LogicActor
+
 object TimeTracker extends App {
-  println("Starting TimeTracer")
+  implicit val system = ActorSystem("TimeTracker")
+
+  val databaseRef = DatabaseActor.createRef
+  val logicRef = LogicActor.createRef(databaseRef)
 }
